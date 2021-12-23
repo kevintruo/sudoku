a5164d510b5dc26d748b511e6827d00ef8143f64
kevintruo <kevintruong088@gmail.com>
2021-12-23 12:42:42 -0500

    Added soduku class

---
+++ Soduku.cs
@@ -0,0 +1,14 @@
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Soduku
+    {
+        private int[,] board;
+
+        public Soduku(int[,] board)
+        {
+            this.board = board;
+        }
+
+        public int[,] Board { get => board; set => board = value; }
+    }
+}
