f53e3880eda3cac24c7af6ca208daf45511648f4
kevintruo <kevintruong088@gmail.com>
2021-12-23 14:18:59 -0500

    Added letters for columns, numbers for rows

--- Sudoku.cs
+++ Sudoku.cs
@@ -22,17 +22,29 @@ namespace MyApp // Note: actual namespace depends on the project name.
             //Traverse the board
             for(int i = 0; i < board.GetLength(0); i++){
                 //Draw a horizontal line every third time 
-                if(i % 3 == 0) Console.WriteLine("-------------------------");
+                if(i % 3 == 0) Console.WriteLine("  -------------------------");
 
                 for(int j = 0; j < board.GetLength(1); j++){
+                    //Add number from 1 to 9 at the start of every row
+                    if(j == 0) Console.Write(i + 1 + " ");
                     //Draw a vertical line every third time
                     if(j % 3 == 0) Console.Write("| ");
-                    if(board[i,j] == 0) Console.Write("_ ");
+                    //Insert "_" instead of 0, and change the colour to dark yellow for better visibility
+                    if(board[i,j] == 0) {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.Write("_ ", Console.ForegroundColor);
+                        Console.ResetColor();
+                    }
+                    //Write provided numebers
                     else Console.Write(board[i,j] + " ");
                 }
+                //End every row with |
                 Console.Write("|\n");
             }
-            Console.WriteLine("-------------------------");
+            //Bottom border
+            Console.WriteLine("  -------------------------");
+            //Add letter from A to J at the end of every column
+            Console.WriteLine("    A B C   D E F   G H J ");
         }
     }
 }
