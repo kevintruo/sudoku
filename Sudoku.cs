436ca8e4ccc04e26951485938393147185258398
kevintruo <kevintruong088@gmail.com>
2021-12-30 16:17:37 -0500

    Added complete board

--- Sudoku.cs
+++ Sudoku.cs
@@ -45,6 +45,19 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 {6,0,0,0,0,0,0,0,0}
         };
 
+        //Completed board
+            int[,] board = new int[,] {
+                {7,9,2,1,5,4,3,8,6},
+                {6,4,3,8,2,7,1,5,9},
+                {8,5,1,3,9,6,7,2,4},
+                {2,6,5,9,7,3,8,4,1},
+                {4,8,9,5,6,1,2,7,3},
+                {3,1,7,4,8,2,9,6,5},
+                {1,3,6,7,4,8,5,9,2},
+                {9,7,4,2,1,5,6,3,8},
+                {5,2,8,6,3,9,4,1,7}
+        };
+
         //Constructor
         public Sudoku(){
             playBoard = new int[,]{};
